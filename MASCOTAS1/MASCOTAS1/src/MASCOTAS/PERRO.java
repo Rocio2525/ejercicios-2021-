37501b2290e70941b8c31b63d1ae21c442503b29
Rocio2525 <71531844+Rocio2525@users.noreply.github.com>
2021-04-04 17:38:28 -0300

    Add files via upload

---
+++ MASCOTAS1/MASCOTAS1/src/MASCOTAS/PERRO.java
@@ -0,0 +1,15 @@
+
+package MASCOTAS;
+
+
+public class PERRO {
+    String nombre;
+    String raza,color;
+    int edad;
+    
+    
+
+public void ladrar(){
+System.out.println("gua gua");
+}
+}
---
+++ MASCOTAS1/MASCOTAS1/src/MASCOTAS/PERROMAIN.java
@@ -0,0 +1,9 @@
+
+package MASCOTAS;
+
+public class PERROMAIN {
+     public static void main(String[] arg){
+     PERRO Olivia=new PERRO();
+     Olivia.ladrar();
+}
+}
