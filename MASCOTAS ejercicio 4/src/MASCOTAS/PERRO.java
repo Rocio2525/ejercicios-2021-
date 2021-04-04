bef8592b80d7eb85facd5be15096733dc54619b8
Rocio2525 <71531844+Rocio2525@users.noreply.github.com>
2021-04-04 20:05:35 -0300

    Add files via upload

---
+++ MASCOTAS ejercicio 4/src/MASCOTAS/PERRO.java
@@ -0,0 +1,25 @@
+
+package MASCOTAS;
+
+
+public class PERRO {
+   String nombre;
+   String raza,color;
+   int edad;
+    
+    public PERRO(String raza, String color){
+this.raza=raza;
+this.color=color;
+
+}
+
+
+public void imprimir (){
+    System.out.println("LA RAZA DE LA MASCOTA es "+raza);
+    System.out.println("EL COLOR  DE LA MASCOTA es "+color);
+}
+
+
+//public void ladrar(){
+//System.out.println("gua gua");
+        }
