4c20ff0e65b926b309a0b03fde31713835633118
Rocio2525 <71531844+Rocio2525@users.noreply.github.com>
2021-04-04 20:30:55 -0300

    Add files via upload

---
+++ LAVARROPA/src/Lavarropas/Lavarropas.java
@@ -0,0 +1,17 @@
+
+package Lavarropas;
+
+public class Lavarropas {
+  private String Modelo = "DF230";
+  private String Marca = "Drean";
+  private String Kilosropa = "12 kilos";
+  
+  public void especificaciones(){
+  System.out.println("Marca: " + Modelo);
+  System.out.println("Modelo: " + Marca);
+  System.out.println("Maximo de Kilos Ropa: " + Kilosropa);
+  
+  
+  }
+  
+}
---
+++ LAVARROPA/src/Lavarropas/Programas.java
@@ -0,0 +1,17 @@
+
+package Lavarropas;
+
+public class Programas {
+   private final int programa;
+   
+   public Programas(int prog){
+   this.programa = prog;
+   }
+    
+   public void display(){
+    System.out.println("El programa Seleccionado es :" + programa);
+   
+   }
+
+    
+}
